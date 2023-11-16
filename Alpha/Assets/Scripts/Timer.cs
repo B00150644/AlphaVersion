c994e75cee910cdf45c4935dd5721bfe5de2885d
B00150644 <B00150644@mytudublin.ie>
2023-11-16 22:33:24 +0000

    AlphaV2

---
+++ Alpha/Assets/Scripts/Timer.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    float timer; // Remove 'static' keyword
+    public GameObject continueButton;
+
+    public Image[] stars;
+    public Text lapText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        continueButton.SetActive(false);
+        ResetTimer(); // Resets timer per level
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+        ResetTimer(); // Reset the timer when the game is restarted
+    }
+
+    void ResetTimer()
+    {
+        timer = 0.0f;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime; // Use Time.deltaTime to increment the timer
+
+        int minutes = Mathf.FloorToInt(timer / 60);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+
+        string time = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        timerText.text = time;
+
+        if (Input.GetKeyDown("9"))
+        {
+            RestartGame();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        lapText.text = "Lap: " + timerText.text;
+        ResetTimer();
+        continueButton.SetActive(true);
+    }
+}
